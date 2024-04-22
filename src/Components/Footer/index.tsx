e2431d96773ad3eefbd6f1a62e1175d2dc19cf17
PoniatowskiAleksander <clakoja@gmail.com>
2024-04-22 15:41:08 +0200

    Dodano style do footera

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,3 +1,5 @@
+import "./footer_style.scss"
+
 
 export function Footer() {
     return (
@@ -5,9 +7,27 @@ export function Footer() {
             <div className="footerLeft">
                 {/*linki do sociali i copyright */}
             </div>
-            <div className="footerRight">
-                {/*linki do stron */}
+            <div className="footerRight"> 
+                <div className="customerService"> 
+                    <p>CUSTOMER SERVICE</p>
+                    <ul className="footerMenuUl">
+                        <li><a href='#'>PROVABLY FAIR</a></li>
+                        <li><a href='#'>TERMS OF SERVICE</a></li>
+                        <li><a href='#'>PRIVACY POLICY</a></li>
+                        <li><a href='#'>SUPPORT</a></li>
+                        <li><a href='#'>FAQ</a></li>
+                    </ul>
+                </div>
+
+                <div className="myAccount">
+                    <p>MY ACCOUNT</p>
+                    <ul className="footerMenuUl">
+                        <li><a href='#'>MY ACCOUNT</a></li>
+                        <li><a href='#'>AFFILIATE SYSTEM</a></li>
+                    </ul>
+                </div>
             </div>
+            
         </div>
     )
 }
